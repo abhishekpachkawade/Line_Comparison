4d7974d8c70ec313134f4fe09e0f5e862c4f248a
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-08-18 08:34:25 +0530

    Welcome Line Comparison

---
+++ LineComparison.java
@@ -0,0 +1,13 @@
+
+//Start with Displaying Welcome to Line Comparison Computation Programa
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		
+		//Print Statement
+		System.out.println("Wellcome Line Comparison");
+
+	}
+
+}
